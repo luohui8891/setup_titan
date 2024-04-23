79d09f3b712ba1fbc62ffddbbd676c48ce56de5e
luohui07 <luohui07@baidu.com>
2024-04-23 13:40:20 +0800

    被官方骗了，开 nmb 一个节点。

--- setup_storage.sh
+++ setup_storage.sh
@@ -4,7 +4,8 @@ read -p "请输入你想要分配每个节点存储（GB）: " storage_gb
 # 設置 Docker 容器 50G
 storage_command="titan-edge config set --storage-size "$storage_gb"GB"
 
-# 现在只有一个节点了
-docker exec -it titan1 bash -c "$storage_command"
+for i in {1..5}; do
+    docker exec -it titan1 bash -c "$storage_command"
+done
 
 docker compose restart
--- setup_titan.sh
+++ setup_titan.sh
@@ -65,8 +65,9 @@ storage_command="titan-edge config set --storage-size "$storage_gb"GB"
 # 绑定 hash
 bind_command="titan-edge bind --hash="$user_hash" https://api-test1.container1.titannet.io/api/v2/device/binding"
 
-# 现在只有一个节点了
-docker exec -it titan1 bash -c "$storage_command"
-docker exec -it titan1 bash -c "$bind_command"
+for i in {1..5}; do
+    docker exec -it titan$i bash -c "$storage_command"
+    docker exec -it titan$i bash -c "$bind_command"
+done
 
 docker compose restart
